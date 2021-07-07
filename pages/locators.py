ccfbab127a1eb6e27a9887cc826c5657482d5e28
Evgeniy Orlov <seaman2774@yandex.ru>
2021-07-07 20:42:18 +0300

    base_page.py: function solve_quiz_and_get_code added with imports locators.py: class ProductPage_Locators added file product_page.py added file test_product_page.py added

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#id_login-username")
     REGISTER_FORM = (By.CSS_SELECTOR, "#id_registration-email")
+
+class ProductPageLocators():
+    BASKET_BUTTON_LINK = (By.CSS_SELECTOR, "form#add_to_basket_form > button.btn-add-to-basket")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "h1 + .price_color")
+    BASKET_PRICE = (By.CSS_SELECTOR, "div.alertinner > p > strong ")
+    PRODUCT_NAME_PAGE = (By.CSS_SELECTOR, "div.row h1")
+    PRODUCT_NAME_BASKET = (By.CSS_SELECTOR, "div.alertinner > strong")
